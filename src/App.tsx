d5eb9b1739b2efbcb3d1d6788f4e617e032e9d9a
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-04-03 10:46:43 -0300

    Added AOS and React Slick Carousel Packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 700,
+      easing: "ease-in",
+      delay: 100,
+    });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="font-bold">Vite + React</h1>
-      <div className="">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="">Click on the Vite and React logos to learn more</p>
     </>
   );

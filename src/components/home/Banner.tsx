b26ad1428c790c0eea6a21002372ed8e24c7d39e
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-04-05 22:34:15 -0300

    A few changes to the Banner

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,5 +1,28 @@
+import bannerImg from "../../assets/coffee-white.png";
+import backgroundImg from "../../assets/coffee-texture.jpg";
+
+const bgImage = {
+  backgroundImage: `url(${backgroundImg})`,
+  backgroundColor: "#270c03",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  height: "100%",
+  width: "100%",
+};
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section style={bgImage}>
+      <div className="container">
+        <div className="flex gap-6">
+          <div>
+            <img src={bannerImg} alt="Banner Image" className="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;

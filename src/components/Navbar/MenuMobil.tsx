6060bd1bcd2b4112f76be3102edeefe430e0f99e
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-04-08 14:47:49 -0300

    Added Menu for Mobil devices

--- src/components/Navbar/MenuMobil.tsx
+++ src/components/Navbar/MenuMobil.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FaCoffee } from "react-icons/fa";
+import { IoMenuSharp } from "react-icons/io5";
 
 type MenuMobilProps = {
   navigation: {
@@ -12,31 +13,20 @@ const MenuMobil: React.FC<MenuMobilProps> = ({ navigation }) => {
   const [menu, setMenu] = useState(false);
 
   return (
-    <div
-      data-aos="fade-down"
-      data-aos-once="true"
-      data-aos-delay="300"
-      className="block sm:hidden items-center gap-7 text-white/80"
-    >
-      <button
-        onClick={() => setMenu(!menu)}
-        className="flex items-center gap-3 bg-primary/70 px-4 py-2 rounded-full hover:scale-105 hover:text-white/90 active:scale-95 transition cursor-pointer"
-      >
-        <FaCoffee className="text-xl" />
+    <div className="flex items-center sm:hidden text-white font-bold text-2xl">
+      <button onClick={() => setMenu(!menu)}>
+        <IoMenuSharp className="text-4xl" />
       </button>
       {menu && (
-        <div className="absolute left-0 top-0 flex flex-col gap-6">
-          <ul className="flex flex-col gap-6">
+        <div className="z-10 absolute left-0 right-0 top-[10vh] h-[90vh] flex flex-col justify-center items-center gap-20 bg-secondary/95">
+          <ul className="flex flex-col gap-20 items-center">
             {navigation.map((data) => (
-              <li
-                key={data.name}
-                className="hover:scale-105 hover:text-white/90 active:scale-95 transition"
-              >
+              <li key={data.name}>
                 <a href={data.path}>{data.name}</a>
               </li>
             ))}
           </ul>
-          <button className="flex items-center gap-3 bg-primary/70 px-4 py-2 rounded-full hover:scale-105 hover:text-white/90 active:scale-95 transition cursor-pointer">
+          <button className="flex items-center gap-3 bg-primary/70 px-4 py-2 rounded-full">
             Order
             <FaCoffee className="text-xl" />
           </button>
